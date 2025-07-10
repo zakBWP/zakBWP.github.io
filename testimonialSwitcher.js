7d7433b2eb74eb7247bf57034f8e5cdcc60db360
Zak <zakmr@icloud.com>
2025-07-10 16:01:24 -0400

    Implemented transitions for testimonial carousel

--- testimonialSwitcher.js
+++ testimonialSwitcher.js
@@ -2,72 +2,62 @@
 
 import testimonials from "./testimonials.js";
 
-let div, card, rightArrow, leftArrow, dotContainer
+let carousel, container, rightArrow, leftArrow, dotContainer
 
 let index = 0
 let carouselTimer = null
 const DELAY_MS = 7200
 
 function getElements() {
-  div = document.querySelector(".testimonials")
+  carousel = document.querySelector(".testimonials__carousel")
+  container = carousel.querySelector(".testimonials__slide-container")
+  rightArrow = carousel.querySelector(".arrow--right")
+  leftArrow = carousel.querySelector(".arrow--left")
+  dotContainer = carousel.querySelector(".testimonials__dot-container")
 
-  // title = div.querySelector(".card__title")
-  // subtitle = div.querySelector(".card__subtitle")
-  // text = div.querySelector(".card__text")
-  card = div.querySelector(".card--testimonial")
-
-  rightArrow = div.querySelector(".arrow--right")
-  leftArrow = div.querySelector(".arrow--left")
-
-  dotContainer = div.querySelector(".testimonials__dot-container")
-
-  if (![div, card, rightArrow, leftArrow, dotContainer].every(Boolean)) {
-    throw new Error("Could not find all testimonial card elements, preserving default innerHTML...")
+  if (![carousel, container, rightArrow, leftArrow, dotContainer].every(Boolean)) {
+    throw new Error("Could not find all testimonial Slide elements, preserving default innerHTML...")
   }
 
-  carouselTimer = setInterval(switchTestimonial, DELAY_MS, true)
+  carouselTimer = setInterval(() => setTestimonial(index + 1), DELAY_MS, true)
 
+  addSlides()
+  addDots()
 
-  let cards = addCards()
+  rightArrow.parentNode.addEventListener("click", () => setTestimonial(index + 1))
+  leftArrow.parentNode.addEventListener("click", () => setTestimonial(index - 1))
 
-  console.log(cards)
-
-  let dots = addDots()
-
-
-  rightArrow.parentNode.addEventListener("click", () => switchTestimonial(true))
-  leftArrow.parentNode.addEventListener("click", () => switchTestimonial(false))
-
-  div.addEventListener("touchstart", () => switchTestimonial(true))
+  container.addEventListener("touchstart", () => setTestimonial(index + 1))
 
 }
 
-function addCards() {
+function addSlides() {
 
   let title, subtitle, text
 
   for (let i = 0; i < testimonials.length; i++) {
 
-    let newCard = document.createElement("article")
-    newCard.classList.add("card", "card--testimonial")
+    let newSlide = document.createElement("article")
+    newSlide.classList.add("testimonials__slide")
 
-    title = newCard.appendChild(document.createElement("h2"))
+    title = newSlide.appendChild(document.createElement("h2"))
     title.classList.add("card__title")
-    subtitle = newCard.appendChild(document.createElement("h3"))
-    subtitle.classList.add("card__subtitle")
-    text = newCard.appendChild(document.createElement("p"))
-    text.classList.add("card__text")
-
     title.textContent = testimonials[i].title
-    subtitle.textContent = testimonials[i].subtitle
-    text.textContent = testimonials[i].textContent
 
-    if (i == 0) newCard.classList.add("card--testimonial--active")
+    subtitle = newSlide.appendChild(document.createElement("h3"))
+    subtitle.classList.add("card__subtitle")
+    subtitle.textContent = testimonials[i].subtitle
+
+    text = newSlide.appendChild(document.createElement("p"))
+    text.classList.add("card__text")
+    text.textContent = testimonials[i].text
+
+    if (i == 0) newSlide.classList.add("testimonials__slide--active")
     
-    div.appendChild(newCard)
+    container.appendChild(newSlide)
   }
 
-  return div.querySelectorAll(".card, .card--testimonial")
+  return container.querySelectorAll(".testimonials__slide")
 }
 
 function addDots() {
@@ -75,11 +65,10 @@ function addDots() {
 
     let newSpan = document.createElement("span")
     newSpan.classList.add("testimonials__dot")
-    newSpan.style.cursor = "pointer"
 
     if (i == 0) newSpan.classList.add("testimonials__dot--active")
     
-    newSpan.addEventListener("touchstart", () => switchTestimonial(true))
+    newSpan.addEventListener("click", () => setTestimonial(i))
 
     dotContainer.appendChild(newSpan)
   }
@@ -87,48 +76,29 @@ function addDots() {
   return dotContainer.children
 }
 
-/* Takes in a boolean, can move forward or backward one index */
-function switchTestimonial(forward) {
-
-  clearInterval(carouselTimer)
-  carouselTimer = setInterval(switchTestimonial, DELAY_MS, true)
-
-  let previousIndex = index
-
-  // Correct for over/underflow
-  index = (index + (forward ? 1 : -1) + testimonials.length) % testimonials.length
-
-
-
-  title.textContent = testimonials[index].title
-  subtitle.textContent = testimonials[index].subtitle
-  text.textContent = testimonials[index].text
-
-  let currentDot = dotContainer.children[index]
-  currentDot.classList.add("testimonials__dot--active")
-
-  let previousDot = dotContainer.children[previousIndex]
-  previousDot.classList.remove("testimonials__dot--active")
-}
-
 /* Takes in an index number, can move anywhere */
 function setTestimonial(desiredIndex) {
 
-  console.log("Setting testimonial to " + desiredIndex)
-
   clearInterval(carouselTimer)
-  carouselTimer = setInterval(switchTestimonial, DELAY_MS, true)
+  carouselTimer = setInterval(() => setTestimonial(index + 1), DELAY_MS, true)
 
   let previousIndex = index
-  console.log("Previous index: " + previousIndex)
-  index = desiredIndex
 
+  if (desiredIndex >= testimonials.length) {
+    index = 0
+  } else if (desiredIndex < 0) {
+    index = testimonials.length - 1
+  } else {
+    index = desiredIndex
+  }
 
-  title.textContent = testimonials[desiredIndex].title
-  subtitle.textContent = testimonials[desiredIndex].subtitle
-  text.textContent = testimonials[desiredIndex].text
+  let currentSlide = container.children[index]
+  currentSlide.classList.add("testimonials__slide--active")
 
-  let currentDot = dotContainer.children[desiredIndex]
+  let previousSlide = container.children[previousIndex]
+  previousSlide.classList.remove("testimonials__slide--active")
+
+  let currentDot = dotContainer.children[index]
   currentDot.classList.add("testimonials__dot--active")
 
   let previousDot = dotContainer.children[previousIndex]
