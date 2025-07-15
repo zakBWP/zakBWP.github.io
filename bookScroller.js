78c6a0cc74fc839fbfcffd54d893c5e341232c04
Zak <zakmr@icloud.com>
2025-07-15 15:58:29 -0400

    Updated metadata

---
+++ bookScroller.js
@@ -0,0 +1,11 @@
+const scroller = document.querySelector(".scroller")
+const inner = scroller.querySelector(".scroller__inner")
+
+
+const scrollerChildren = Array.from(inner.children);
+
+for (let i = 0; i < scrollerChildren.length; i++) {
+  const item = inner.children[i]
+  const duplicatedItem = item.cloneNode(true);
+  inner.appendChild(duplicatedItem);
+}
