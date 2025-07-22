3b9717127d8d7c6e63ac9b0ca016b44b0d4f3ddc
Zak <zakmr@icloud.com>
2025-07-22 12:22:54 -0400

    Populated certificate pages

---
+++ scripts/lightbox.js
@@ -0,0 +1,47 @@
+let wrappers, overlay, overlayImage, overlayCaption;
+
+function getElements() {
+  wrappers = document.querySelectorAll(".lightbox-wrapper")
+
+  overlay = document.querySelector(".overlay")
+  overlayImage = overlay.querySelector(".overlay__photo")
+  overlayCaption = overlay.querySelector(".overlay__caption")
+  // x = overlay.querySelector(".overlay__x")
+
+  
+  if (![wrappers, overlay, overlayImage, overlayCaption].every(Boolean)) {
+    throw new Error("Could not find all Lightbox elements!")
+  }
+  
+  let certificatePhoto, certificateSpan
+
+  wrappers.forEach((wrapper) => {
+
+    wrapper.addEventListener("click", () => {
+      toggleOverlay()
+
+      certificatePhoto = wrapper.querySelector(".certificate-photo")
+      certificateSpan = wrapper.querySelector(".lightbox-filter")
+
+      overlayImage.src = certificatePhoto.src
+      overlayImage.alt = certificateSpan.textContent
+      overlayCaption.textContent = certificateSpan.textContent
+        
+    })
+  })
+  overlay.addEventListener("click", toggleOverlay)
+}
+
+function toggleOverlay() {
+  console.log("toggling...")
+  overlay.classList.toggle("show")
+}
+
+(function() {
+  try {
+    getElements()
+  } catch(error) {
+    console.error(error)
+    return
+  }
+})()
