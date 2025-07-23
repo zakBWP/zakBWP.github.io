071216d80e31315fadaa324b95b3c97dd0ada05b
Zak <zakmr@icloud.com>
2025-07-23 14:56:16 -0400

    Fixed scroller on Safari

--- scripts/bookScroller.js
+++ scripts/bookScroller.js
@@ -1,10 +1,17 @@
-const scroller = document.querySelector(".scroller")
-const inner = scroller.querySelector(".scroller__inner")
+window.addEventListener("load", () => {
+  const scroller = document.querySelector(".scroller");
+  const inner = scroller.querySelector(".scroller__inner");
+  const content = Array.from(inner.children);
 
-const scrollerChildren = Array.from(inner.children);
+  for (let i = 0; i < content.length; i++) {
+    const item = content[i];
+    const duplicatedItem = item.cloneNode(true);
+    duplicatedItem.setAttribute("aria-hidden", true);
+    inner.appendChild(duplicatedItem);
+  }
 
-for (let i = 0; i < scrollerChildren.length; i++) {
-  const item = inner.children[i]
-  const duplicatedItem = item.cloneNode(true);
-  inner.appendChild(duplicatedItem);
-}
+  // Force reflow & restart animation
+  inner.style.animation = "none";
+  inner.offsetHeight;
+  inner.style.animation = "";
+});
