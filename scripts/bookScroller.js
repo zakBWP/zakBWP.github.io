ade047e6de98e959a86ba4cf56e12a3171f9d598
Zak <zakmr@icloud.com>
2025-07-24 08:51:21 -0400

    misc

--- scripts/bookScroller.js
+++ scripts/bookScroller.js
@@ -1,17 +1,17 @@
 window.addEventListener("load", () => {
-  const scroller = document.querySelector(".scroller");
-  const inner = scroller.querySelector(".scroller__inner");
-  const content = Array.from(inner.children);
+  const scroller = document.querySelector(".scroller")
+  const inner = scroller.querySelector(".scroller__inner")
+  const content = Array.from(inner.children)
 
   for (let i = 0; i < content.length; i++) {
-    const item = content[i];
-    const duplicatedItem = item.cloneNode(true);
-    duplicatedItem.setAttribute("aria-hidden", true);
-    inner.appendChild(duplicatedItem);
+    const item = content[i]
+    const duplicatedItem = item.cloneNode(true)
+    duplicatedItem.setAttribute("aria-hidden", true)
+    inner.appendChild(duplicatedItem)
   }
 
-  // Force reflow & restart animation
-  inner.style.animation = "none";
-  inner.offsetHeight;
-  inner.style.animation = "";
+  // Hack to fix Safari animation rendering
+  inner.style.animation = "none"
+  inner.offsetHeight
+  inner.style.animation = ""
 });
